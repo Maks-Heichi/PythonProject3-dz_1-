c4a92e2ec828e72ae31d3a33822c281eaf7b34b1
Maksimka <mister.max1988@yandex.ru>
2026-01-29 22:45:07 +0500

    git commit tests

---
+++ tests/test_widget.py
@@ -0,0 +1,12 @@
+from src.widget import mask_account_card, get_date
+import pytest
+
+@pytest.mark.parametrize(x, y [
+    ('Visa Platinum 7000792289606361', 'Visa Platinum 7000 79** **** 6361')
+    ('Счет 73654108430135874305','Счет **4305')
+])
+def test_mask_account_card(x, y):
+    assert mask_account_card(x) == y
+
+def test_get_date():
+    assert get_date('2026-01-04T013:00:00.671407') == '04.01.2026'
