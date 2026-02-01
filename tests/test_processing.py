13c4405dc7226c719f89b037c9c4bd83960f350e
Maksimka <mister.max1988@yandex.ru>
2026-02-01 15:16:52 +0500

    git commit test

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -16,12 +16,12 @@ def test_filter_by_state(transactions_list):
     executed_transactions = filter_by_state(transactions_list, 'EXECUTED')
     assert executed_transactions == [
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     ]
     canceled_transactions  = filter_by_state(transactions_list, 'CANCELED')
     assert canceled_transactions  == [
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
     ]
 
 
@@ -30,5 +30,5 @@ def test_sort_by_date(transactions_list):
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     ]
