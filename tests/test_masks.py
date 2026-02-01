8c88257cdb18ec6293de11d71b721d8b27025b1d
Maksimka <mister.max1988@yandex.ru>
2026-02-01 14:28:55 +0500

    git commit test

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,8 +1,10 @@
 from main.py import get_mask_card_number, get_mask_account
 import pytest
 
+
 def test_get_mask_card_number():
     assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
+
 
 def test_get_mask_account():
     assert get_mask_account("73654108430135874305") == "**4305"
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,7 @@
 from src.processing import filter_by_state, sort_by_date
 import pytest
 
+
 @pytest.fixture
 def transactions_list():
     return [
@@ -9,6 +10,7 @@ def transactions_list():
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ]
+
 
 def test_filter_by_state(transactions_list):
     executed_transactions = filter_by_state(transactions_list, 'EXECUTED')
@@ -21,6 +23,7 @@ def test_filter_by_state(transactions_list):
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ]
+
 
 def test_sort_by_date(transactions_list):
     assert  sort_by_date(transactions_list) == [
