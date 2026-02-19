550cc5d60d194f662b2252db410d2cd4e06433bb
Maksimka <mister.max1988@yandex.ru>
2026-02-19 18:46:41 +0500

    git commit

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,9 +4,20 @@ import json
 from src.utils import load_transactions
 
 
-@patch('builtins.open', new_callable=MagicMock)
-def test_load_transactions(self, mock_open):
-    mock_open.return_value.__enter__.return_value.read.return_value = json.dumps([{"amount": 100, "currency": "USD"}])
-    result = load_transactions('data/operations.json')
-    self.assertEqual(len(result), 1)
-    self.assertEqual(result[0]['amount'], 100)
+class TestLoadTransactions(unittest.TestCase):
+
+    @patch('builtins.open', new_callable=MagicMock)
+    def test_load_transactions(self, mock_open):
+        mock_open.return_value.__enter__.return_value.read.return_value = json.dumps(
+            [{"amount": 100, "currency": "USD"}])
+
+        # Вызов функции для тестирования
+        result = load_transactions('data/operations.json')
+
+        # Проверки
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0]['amount'], 100)
+
+
+if __name__ == '__main__':
+    unittest.main()
