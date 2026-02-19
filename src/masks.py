24d4534bf278c5176ffa8f1fe0191fb72b4f54c7
Maksimka <mister.max1988@yandex.ru>
2026-02-19 07:13:07 +0500

    git commit

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,6 @@
+import logging
+
+
 def get_mask_card_number(card):
     """
         Формирует маску номера банковской карты из 16 цифр.
@@ -32,3 +35,24 @@ def get_mask_account(card):
         print('карта должна содержать 20 чисел')
     mask = f'**{card[-4:]}'
     return mask
+
+
+logger = logging.getLogger(__name__)
+# Создаем хендлер для вывода в файл
+file_handler = logging.FileHandler('logs/masks.log')
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='masks.log', filemode='w')
+file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+logger.debug('Debug message')
+logger.info('Info message')
+logger.warning('Warning message')
+logger.error('Error message')
+logger.critical('Critical message')
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 from typing import List, Dict, Any
 
@@ -14,3 +15,24 @@ def load_transactions(operations: str) -> List[Dict[str, Any]]:
         return [] # Если файл не найден или данные не являются корректным JSON, возвращаем пустой список
 
     return [] # На случай, если данные не являются списком
+
+
+logger = logging.getLogger(__name__)
+# Создаем хендлер для вывода в файл
+file_handler = logging.FileHandler('logs/utils.log')
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='utils.log', filemode='w')
+file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+logger.debug('Debug message')
+logger.info('Info message')
+logger.warning('Warning message')
+logger.error('Error message')
+logger.critical('Critical message')
