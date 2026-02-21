8c53add4401970aaa2c5495c8a76e4f124239a2c
Maksimka <mister.max1988@yandex.ru>
2026-02-21 23:55:26 +0500

    git commit

--- src/utils.py
+++ src/utils.py
@@ -13,6 +13,7 @@ logger.setLevel(logging.DEBUG)
 
 def load_transactions(operations: str) -> List[Dict[str, Any]]:
     """Загружает транзакции из JSON-файла."""
+    logger.info("Загружается транзакция")
     try:
         with open(
             operations,
@@ -21,8 +22,16 @@ def load_transactions(operations: str) -> List[Dict[str, Any]]:
         ) as file:
             data = json.load(file)
             if isinstance(data, list):
+                logger.info("Транзакция успешно загружена")
                 return data
-    except (FileNotFoundError, json.JSONDecodeError):
-        return []  # Если файл не найден или данные не являются корректным JSON, возвращаем пустой список
+
+    except FileNotFoundError:
+        error_msg = "Файл не найден"
+        logger.error(error_msg)
+        raise ValueError(error_msg)
+    except json.JSONDecodeError:
+        error_msg = "Данные не являются корректным"
+        logger.error(error_msg)
+        raise ValueError(error_msg)
 
     return []  # На случай, если данные не являются списком
