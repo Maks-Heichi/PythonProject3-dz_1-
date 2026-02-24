26104f48dd8178dbedbd303df35c9ef6d8cf2088
Maksimka <mister.max1988@yandex.ru>
2026-02-24 19:54:11 +0500

    git commit test

---
+++ tests/test_finance_operations_csv.py
@@ -0,0 +1,27 @@
+import csv
+import unittest
+
+from unittest.mock import mock_open, patch
+from typing import List, Dict
+
+from src.finance_operations_csv import read_transactions_from_csv
+
+
+# Тестирование функции
+class TestReadTransactionsFromCsv(unittest.TestCase):
+
+    @patch("builtins.open", new_callable=mock_open, read_data="id,amount,date\n1,100,2023-01-01\n2,200,2023-01-02")
+    def test_read_transactions(self, mock_file):
+        result = read_transactions_from_csv("dummy_path.csv")
+
+        expected = [
+            {"id": "1", "amount": "100", "date": "2023-01-01"},
+            {"id": "2", "amount": "200", "date": "2023-01-02"}
+        ]
+
+        self.assertEqual(result, expected)
+
+
+# Запуск тестов
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_finance_operations_exl.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import unittest
+from unittest.mock import patch, MagicMock
+
+from src.finance_operations_exl import read_transactions_from_excel
+
+
+# Тестирование функции
+class TestReadTransactionsFromExcel(unittest.TestCase):
+
+    @patch('pandas.read_excel')
+    def test_read_transactions(self, mock_read_excel):
+        # Создаем имитацию DataFrame с тестовыми данными
+        test_data = {
+            'id': [1, 2],
+            'amount': [100, 200],
+            'date': ['2023-01-01', '2023-01-02']
+        }
+        mock_df = pd.DataFrame(test_data)
+        mock_read_excel.return_value = mock_df
+
+        result = read_transactions_from_excel("dummy_path.xlsx")
+
+        expected = [
+            {"id": 1, "amount": 100, "date": "2023-01-01"},
+            {"id": 2, "amount": 200, "date": "2023-01-02"}
+        ]
+
+        self.assertEqual(result, expected)
+
+
+# Запуск тестов
+if __name__ == "__main__":
+    unittest.main()
