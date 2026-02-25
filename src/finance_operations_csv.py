cc210ece7eae8c72b0f21c5ffb5d1cd4b84ac980
Maksimka <mister.max1988@yandex.ru>
2026-02-25 19:35:41 +0500

    git commit

--- src/finance_operations_csv.py
+++ src/finance_operations_csv.py
@@ -2,17 +2,17 @@ import csv
 from typing import Dict, List
 
 
-def read_transactions_from_csv(file_path: str) -> List[Dict]:
+def read_transactions_from_csv(file_path: str, delimiter: str = ",") -> List[Dict]:
     """Чтение CSV-файла и возврат списка словарей"""
     transactions = []
     with open(file_path, "r", encoding="UTF-8") as file:
-        reader = csv.DictReader(file)
+        reader = csv.DictReader(file, delimiter=delimiter)
         for row in reader:
             transactions.append(row)  # Добавляем каждую строку в список
     return transactions
 
 
 # Пример использования функции
-csv_transactions = read_transactions_from_csv("transactions.csv")
+csv_transactions = read_transactions_from_csv("transactions.csv", delimiter=";")  # Укажите нужный разделитель
 for transaction in csv_transactions:
     print(transaction)
